a5f83957a878800f225849832d6dab21ca82c70c
johnsonli010801 <97709195+johnsonli010801@users.noreply.github.com>
2022-02-03 20:41:35 -0800

    Update skilldem1Test.java

--- skilldem1Test.java
+++ skilldem1Test.java
@@ -3,6 +3,6 @@ import org.junit.*;
 public class skilldem1Test {
     @Test
     public  void divisibleTest(){
-        assertEquals(true,skilldem1.divisible(2,3));//failure test case
+        assertEquals(true,skilldem1.divisible(3,3));//fix the case to be true.
     }
 }
