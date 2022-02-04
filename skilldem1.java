c3da0a5f0879baf3f7a19dfc7d84ff6d65d22695
johnsonli010801 <97709195+johnsonli010801@users.noreply.github.com>
2022-02-03 20:38:35 -0800

    new change skill demo1

---
+++ skilldem1.java
@@ -0,0 +1,13 @@
+public class skilldem1{
+    public static boolean divisible(double a, double b)
+    {
+        if(a%b==0)
+        {
+            return true;
+
+        }
+        else{
+            return false;
+        }
+    }
+}
---
+++ skilldem1Test.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class skilldem1Test {
+    @Test
+    public  void divisibleTest(){
+        assertEquals(true,skilldem1.divisible(2,3);//failure test case
+    }
+}
